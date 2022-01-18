1fd1967a1dc6423690b69b6cf75228877b73408f
Paulo Henrique <76111995+paulopradella@users.noreply.github.com>
2022-01-18 09:44:24 -0300

    sql

---
+++ MySql Trabalhando com as suas primeiras tabelas/DadosVinhos.sql
@@ -0,0 +1,167 @@
+insert into mydb.Regiao (nomeRegiao, descricaoRegiao) values
+("Minho", "O Minho é uma região produtora de vinhos brancos muito típicos, únicos em seu estilo.
+Leves, frutados e refrescantes, de ótima acidez, que alcançam um toque de classe quando elaborados por um produtor talentoso.
+Região vinícola mais setentrional de Portugal, o Minho é conhecido, especialmente, pela produção dos tradicionais Vinhos Verde.
+A proximidade do Minho com o oceano Atlântico é a razão pela qual seus solos são tão prolíferos para a agricultura, em especial, a vinicultura.
+As chuvas são carregadas pelos ventos provenientes do oceano, permitindo que as vinhas cultivadas no Minho apresentem maiores rendimentos
+do que as vinhas cultivadas em regiões portuguesas mais secas, como em Transmontano, por exemplo.
+Além do DOC Vinho Verde, a região dá origem a vinhos rotulados sob o título IGP, cujas leis são mais flexíveis e
+permitem que os produtores ampliem seus portfólios. Antigamente, a IGP Minho era conhecida como Rios do Minho, no entanto,
+a partir de 2008 tal categoria foi modificada para IGP, utilizada nos dias de hoje.
+A região do Minho possui este nome devido ao rio Minho, um importante afluente local que nasce nas colinas da vizinha Galiza,
+fazendo fronteira entre a Espanha e Portugal. Não apenas servindo como uma fonte vital para a região, o rio Minho proporciona condições ideais
+para que as uvas atinjam o ápice qualitativo, adicionando caráter único e marcante aos vinhos.
+Enquanto no extremo norte encontra-se o rio Minho, a parte sul é banhada pelo tradicional rio Douro,
+que ocupa uma posição estratégica no mapa dos bons vinhos portugueses, ou seja, o rio Douro é responsável
+por ligar Portugal e Espanha e a cidade do Porto com os vinhedos do Vale do Douro."),
+
+("Utiel-Requena","Localizada na província de Valência, a 70 km do mar Mediterrâneo, a região, que recebe o nome de duas cidades próximas,
+Utiel e Requena, ocupa um grande planalto circular de aproximadamente 45 km de diâmetro.
+As altitudes variam de 600 a 900 metros acima do nível do mar e a região é cercada por duas cadeias de montanhas.
+A região apresenta dois tipos principais de solo: aluvial e argiloso, com depósitos de calcário.
+O clima continental, definido por invernos longos e frios e verões relativamente curtos, secos e quentes, é homogêneo em toda a região.
+Utiel-Requena também se recebe influência do Mediterrâneo e dos ventos quentes e secos que sopram do Oeste.
+As condições são extremas, mas consideradas ótimas para o cultivo da uva Bobal.
+Assim, a área possui mais de 40.000 hectares (99.000 acres) de vinhedos e uma tradição vitivinícola profundamente enraizada.
+Descobertas arqueológicas mostram que a produção de vinho na região remonta há cerca de 2.700 anos. "),
+
+("Valdapeñas","A região demarcada para a produção dos vinhos dessa denominação abrange vários municípios da província de Ciudade Real:
+Valdepeñas, Alcubillas, Moral de Calatrava, San Carlos del Valle, Santa Cruz de Mudela, e Torrenueva, além de parte dos
+municípios de Torre de Juan Abad, Granátula de Calatrava, Alhambra, e Montiel.
+Trata-se de uma região muito propícia para o cultivo da vinha, com um clima continental de baixo índice pluviométrico,
+e que registra temperaturas extremas, com máximas que superam os 40°C, e mínimas que podem ser inferiores a -10°C.
+Contabilizam-se, em Valdepeñas, 2.500 horas de Sol, por ano.
+Os solos, pobres em matéria orgânica e de baixa fertilidade, obrigam as raízes das videiras a se desenvolverem,
+a se aprofundarem e a se fortalecerem.
+Nesse cenário, cultivam-se uvas que alcançam boa maturação, e que são capazes de produzir vinhos muito estruturados, complexos,
+aromáticos e de coloração muito profunda.
+Anualmente, são produzidos cerca de 57 milhões de litros de vinho rotulados com a denominação Valdepeñas,
+sendo que quase 40% da produção é destinada à exportação da Espanha para outros países.
+A grande maioria dos vinhos de Valdepeñas, cerca de 77% deles, são tintos. Os brancos representam cerca de 18%,
+e os rosés de Valdepeñas, tradicionalmente famosos, representam apenas 5%.
+A notoriedade de Valdepeñas encontra sua origem em vinhos rosés do passado, que foram lentamente dando lugar a tintos elegantes,
+sempre aveludados e frutados, que podem ser jovens ou envelhecidos em barris de carvalho.
+A variedade mais importante de Valdepeñas é sem dúvida, a Tempranillo, que também é conhecida nessa região como Cencibel.
+Outras uvas autorizadas para cultivo em Valdepeñas são as tintas Garnacha, Cabernet Sauvignon, Merlot, Syrah e Petit Verdot,
+além das brancas Airén, Macabeo, Chardonnay, Sauvignon Blanc, Moscatel de Grano Menudo e Verdejo."),
+
+("Borgonha", "Bourgogne ou Borgonha fica no leste da França e vai desde Chablis, ao norte, até a região do Beujolais, ao norte de Lyon.
+É uma região lendária que oferece vinhos brancos e tintos que estão entre os melhores e mais famosos do mundo.
+Quando são produzidos por um bom produtor, são vinhos de incrível elegância e do mais alto nível.
+Terra natal das uvas Pinot Noir e Chardonnay, a Borgonha fica no coração da França, a aproximadamente duas horas de Paris e
+está a 220 metros de altitude média. Com clima continental de invernos rigorosos e geadas em algumas épocas do ano,
+reúne características básicas para cultivar outros ótimos tipos de uvas além destas duas nativas, como a Pinot Gris,
+Pinot Blanc, Gamay, Sauvignon Blanc, Aligoté e Melon de Bourgogne.
+A Borgonha divide-se em diferentes sub-regiões vinícolas, cada uma com a sua particularidade.
+Em Chablis e Grand Auxerrois, a protagonista é a uva Chardonnay, já Côte de Nuits representa o reino dos vinhos tintos produzidos a
+base da Pinot Noir. Châtillonnais, por exemplo, simboliza a sub-região vinícola que produz, exclusivamente, maravilhosos e
+prestigiados vinhos espumantes, como os Crémant de Bourgogne.
+De maneira quase que absoluta, as variedades de uvas encontradas com maior facilidade na região francesa são as castas Pinot Noir e Chardonnay,
+ citadas anteriormente. A Chardonnay ocupa cerca de 48% dos vinhedos, enquanto a Pinot Noir, 34%.
+ O restante é completado pela uva Gamay, 10%, e Aligoté, com 6%.
+Devido aos mais de 27 mil hectares de vinhas, representando 3% do total de vinhedos da França, Borgonha produz, anualmente,
+cerca de 200 milhões de garrafas de vinho, exprimindo a importância da área para o mundo vinícola.
+A maior parte dos vinhos de Bourgogne são exemplares brancos, totalizando 62% da produção, seguido por 30% de rosés ou tintos
+e 8% de espumantes. Além disso, os vinhos da região são classificados em mais de 100 denominações diferentes,
+chamadas na França de Appellations d’Origine Contrôlée, graças a enorme diversidade de condições naturais encontrada na região."),
+
+("Toscana", "Brunello di Montalcino, Chianti e Vino Nobile di Montepulciano são os mais famosos vinhos produzidos na Toscana,
+uma das regiões vinícolas mais importantes da Itália. Formada por mais de 60 mil hectares de vinhedos,
+é responsável por produzir aproximadamente 3 milhões de hectolitros de vinhos por ano.
+A região italiana da Toscana é excelente para o cultivo de uvas, graças a sua geografia acidentada.
+O plantio dos vinhedos em encostas e pontos mais altos proporcionam uma boa incidência solar sobre as vinhas,
+possibilitando o amadurecimento perfeito das frutas. Além disso, a ampla variação de temperatura e os rigorosos invernos mediterrâneos
+também contribuem para a alta qualidade as uvas.
+A protagonista da região é a uva Sangiovese, base para a produção de alguns dos melhores vinhos tintos da Itália, em especial,
+o prestigiado vinho Brunello di Montalcino – Brunello refere-se ao nome como essa variedade de uva é conhecida em Montalcino.
+Na região é possível encontrar as variedades de uvas tintas Canaiolo, Prugnolo Gentile, Morelino di Scansano e Pollera Nera,
+bem como as variedades francesas Merlot e Cabernet Sauvignon. Entre as uvas brancas, as castas Trebbiano, Chardonnay,
+Sauvignon Blanc e Vermentino di Luni são as mais cultivadas.");
+
+insert into mydb.Vinicola (nomeVinicola, descricaoVinicola,foneVinicola,emailVinicola,Regiao_codRegiao) values
+("Casa de Vila Nova", "A Casa de Vila Nova posiciona-se no mercado de Vinho Verde como o produtor Bernardo Lencastre que alia a tradição e experiência dos fundadores,
+à vivacidade e inovação de uma nova geração. Tem como missão oferecer ao mercado vinhos verdes jovens, frescos e de qualidade,
+que permita aos seus clientes proporcionar uma oferta diferenciadora ao consumidor.
+Pretende ser reconhecida como uma Casa de Vinho Verde de qualidade e inovação,
+apostando nas castas de Alvarinho, Avesso, Loureiro, Arinto, Fernão Pires e ainda Chardonnay e Sauvignon Blanc.",
+"+351255732452", "geral@casadevilanova.com", 1),
+
+("Bodegas Torre Oria","Bodegas Torre Oria está localizada na região espanhola de Utiel-Requena (sudeste de Valência).
+No final do século XIX, a família Oria de Rueda dedicou-se com sucesso ao negócio da seda em Valência.
+Anos depois, a família decidiu mudar seu negócio para a cidade de Requena, a 60 km de distância, para o interior de Valência.
+Com o declínio da indústria da seda, eles decidiram converter seus negócios no mundo do vinho.","+34962320289", "info@torreoria.es", 2),
+
+("La Viña", "Foi criada em 1945, quando um grupo de 38 empresários uniu forças,
+reunindo uma área de cultivo de 47 hectares com uma produção de 250.000 kg de uvas.
+A decisão foi extremamente bem sucedida, visto que ao longo das gerações a atividade vitivinícola tem vindo a ganhar importância
+e a adega tem vindo a crescer em número de parceiros e produção.
+Atualmente são cultivados 2.400 hectares, com uma produção anual de 11,3 mil quilos de uvas.
+Como predominam as variedades de uvas vermel. A localização dos seus vinhos favorece a produção de vinhos tintos de qualidade,
+de cor intensa e grande corpo, fruto de longas macerações.","+34962290078","infoweb@vinosdelavina.com",3),
+
+("Pierre Amiot & Fils","O domaine Pierre Amiot foi estabelecido em Morey-Saint-Denis
+5 gerações atrás. Depois de Pierre, Léon e Édouard, Pierre Amiot
+decidiu assumir o controle do domaine em 1951. Ele juntou forças com seu filho mais velho
+Jean-Louis em 1980. O filho mais novo, Didier, começou a trabalhar no domaine em 1991.
+Jean-Louis e Didier cultivam 8 hectares de vinhedos em Morey-Saint-Denis e Gevrey-Chambertin.
+O domínio está distribuído pela propriedade original, que se encontra no centro da vila.
+Existem agora 40 parcelas de vinhas, com 85% em Morey-Saint-Denis e 15% em Gevrey-Chambertin.
+Jean-Louis e Didier têm o orgulho de apresentar uma gama completa de vinhos de todos os níveis de denominação:
+2 Bourgognes, 3 Villages, 5 Premiers Crus e 2 Grands Crus.","+33(0)688385295", "contact@domainepierreamiot.fr", 4 ),
+
+("Fattoria Fèlsina", "Um “exemplo de classe e elegância”,
+segundo o Gambero Rosso — com nada menos que 24 “tre bicchieri” do famoso guia — a Fattoria di Fèlsina
+é um dos ícones do vinho italiano e uma das maiores referências em Chianti Classico.
+Domenico Poggiali comprou a propriedade de Fèlsina em 1966 e, num ato corajoso para a época,
+quando a viticultura italiana passava por um momento difícil, decidiu investir na qualidade dos vinhos e no talento de alguns jovens enólogos.
+Apaixonados pelo universo dos vinhos e grandes empreendedores, Domenico e seu filho Giuseppe Poggiali modernizaram as
+diretrizes da propriedade sem deixar de lado as tradições. Em alguns anos, os hectares cultivados se multiplicaram e transformaram
+toda a organização e a alma dos negócios da Fattoria.
+Em meados dos anos de 1970, Giuseppe Mazzocolin, genro de Domenico, se uniu ao time da vinícola com a missão de desenvolve-la comercialmente.
+Sua base e cultura humanista e consciência da responsabilidade e papel das pessoas nos afazeres da empresa contribuíram para
+o crescimento e propagação da cultura italiana do vinho pelo mundo, obtendo grande reconhecimento internacional.
+A amizade com Luigi Veronelli e a colaboração com o enólogo Franco Bernabei traçaram os caminhos que norteariam a empresa rumo ao futuro.
+Com grande coerência em seus exemplares, a Fattoria chegou ao ano de 1983 lançando os rótulos Fontalloro e Rancia,
+que seguem até hoje entre seus melhores vinhos.
+Em 1998, Giovanni (neto de Domenico) e seu pai Giuseppe Poggiali inovam mais uma vez e investem na tecnologia de
+vinificação em tanques de aço inoxidável, técnica que permite um trabalho muito mais preciso de enólogos e
+viticultores em matéria de equilíbrio, coerência e estilo. Essa grande transformação na forma de elaborar e envelhecer os vinhos e
+a colaboração constante com grandes enólogos e técnicos não influenciaram na tradição da cantina, mas trouxeram ainda mais qualidade à produção.",
+"+390577355117", "info@felsina.it", 5);
+
+insert into mydb.Vinho (nomeVinho, tipoVinho, anoVinho, descricaoVinho, Vinicola_codVinicola) values
+("Levity Branco", "Vinho Verde Branco", 2019, "Um vinho leve que remete seus aromas e
+paladares a frutas cítricas e maçã verde.", 1),
+
+("Levity Rosé", "Vinho Verde Rosé", 2019, "Cor rosé claro, com aroma de frutas vermelhas, morango e cereja.
+De corpo leve e delicado, possui uma boa acidez. É refrescante e fácil no paladar.", 1),
+
+("Alice en l Pais de las Uvas", "Rosé", "2017", "“Não é um sonho, é uma memória. Esse mundo é real.” - Alice no País das Maravilhas.
+Para Alice, uma chave equivale a uma passagem para o país mágico das maravilhas, mas no caso deste vinho,
+ele é sua passagem para os solos da Espanha. Inspirado do filme e feito com a uva Bobal que é muito cultivada em Valência,
+é refrescante e frutado, ou seja, tem tudo que se espera de um rótulo dessa região.",2),
+
+("Anciano Gran Reserva - 10 anos Tempranillo", "Tinto", 2008, "Uvas Tempranillo colhidas manualmente de vinhas com mais de 30 anos,
+cultivadas na Denominação de Origem Valdepeñas, deram vida ao Anciano Gran Reserva. Este best seller espanhol traz à taça
+um raro buquê que mescla notas de café, pimenta e chocolate ao leite em um fundo delicadamente herbáceo. Na boca,
+nos presenteia com goles muito bem equilibrados entre taninos, álcool e acidez,
+características adquiridas graças aos seus 10 anos de amadurecimento — sendo 18 meses em barricas de carvalho e o restante em garrafa — ,
+que vão conquistar seu paladar assim como conquistou o da crítica especializada", 3),
+
+("Clos de La Roche Gran Cru Magnum 2018", "Tinto", 2018,"Aroma intenso cheio de frutas e especiarias.
+Ameixas, amoras, cerejas e anis. Com noz moscada e uma presença terrosa, delineado por ótima presença de madeira.
+Na boca é denso e poderoso, um exemplar clássico desse Grand Cru." , 4),
+
+("Pepestrino", "Branco", 2017,"Elaborado com uma saborosa combinação das castas Trebiano, Chardonnay e Sauvignon Blanc,
+o Pepestrino foi criado para ser uma ótima escolha para iniciar uma refeição. Fresco e bastante aromático,
+pode acompanhar peixes ou mesmo ser bebericado sozinho.", 5);
+
+select nomeVinho, anoVinho, nomeVinicola, nomeRegiao from mydb.Vinho
+	join mydb.Vinicola on Vinicola_codVinicola = codVinicola
+		join mydb.Regiao on Regiao_codRegiao = codRegiao;
+
+create user somellier@"localhost" identified by "1234"
+	 with MAX_QUERIES_PER_HOUR 40;
+
+grant select on mydb.Vinho to somellier@"localhost";
+grant select (codVinicola, nomeVinicola) on mydb.Vinicola to somellier@"localhost";
